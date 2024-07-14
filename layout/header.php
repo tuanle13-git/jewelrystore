b372e91e157ad18a3971759e34b9852f0406bf40
tuanle13-git <tuanlv1303@gmail.com>
2024-07-14 20:18:51 +0700

    thêm layout

---
+++ layout/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="vi">
+
+<head>
+    <meta charset="uft-8">
+    <title>Jewelry</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link href="/public/bootstrap/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="/public/bootstrap/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <script src="/public/js/jquery-3.7.1.min.js"></script>
+    <script src="/public/js/event.js"></script>
+
+    <link href='https://fonts.googleapis.com/css?family=Be Vietnam' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    </style>
+</head>
